2c751bac5b00a8aef301143f885b1b7e94d4d492
antonioramiro <antonio.ramiro@tecnico.ulisboa.pt>
2020-07-19 20:41:42 +0100

    verifying dataset groups

---
+++ sorter.py
@@ -0,0 +1,17 @@
+import os
+import sys #manipulating function arguments
+
+directory = sys.argv[1]
+dataset = {}
+folderSize = len(os.listdir(directory))
+i = 0
+
+while i != folderSize
+    line = os.listdir(directory)[i]
+    if not str(line[11:-5]) in dataset:
+        dataset[str(line[11:-5])] = [i,0]
+    else:
+        dataset[str(line[11:-5])][1] = i
+
+for x, y in dataset.items():
+  print(x, y)
