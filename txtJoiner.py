bd3c70a697e8c21481450390bde50dc0873f8e67
antonioramiro <antonio.ramiro@tecnico.ulisboa.pt>
2020-07-19 21:19:20 +0100

    debug2

--- txtJoiner.py
+++ txtJoiner.py
@@ -6,6 +6,9 @@ with open("dataset_" + str(date.today()) + ".txt","w+") as dataset:
     dir = sys.argv[1]
     files = [os.listdir(dir).sort()]
 
+    print(type(files))
+    print(type(files[9]))
+
     for file in files :
-            lines = open(os.path.join( dir, file ) ,"r").read()
+            lines = open(os.path.join( dir, file) ,"r").read()
             dataset.write(lines+'\n')
