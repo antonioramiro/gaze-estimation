c47e63bc85c61fe8eb5692e453832b059c2589fe
antonioramiro <antonio.ramiro@tecnico.ulisboa.pt>
2020-07-19 21:28:19 +0100

    debug 5

--- txtJoiner.py
+++ txtJoiner.py
@@ -5,16 +5,16 @@ import sys #manipulating function arguments
 with open("dataset_" + str(date.today()) + ".txt","w+") as dataset:        
     dir = sys.argv[1]
     files = os.listdir(dir)
-    print('files',files)
-    print('files type',type(files))
-    files = [files.sort()]
-    print('files type', type(files))
-    print('files', files)
+    print('1 files type',type(files))
+    print('1 files',files)
+    files = sorted(files)
+    print('2 files type', type(files))
+    print('2 files', files)
 
     for file in files :
 
-            print('file',file)
-            print('file',type(file))
+            print('3 file',file)
+            print('3 file',type(file))
 
             lines = open(os.path.join( dir, file) ,"r").read()
             dataset.write(lines+'\n')
