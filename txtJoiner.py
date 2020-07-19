2b8ab5938b89d876d750522994387960645cc985
antonioramiro <antonio.ramiro@tecnico.ulisboa.pt>
2020-07-19 21:17:11 +0100

    funciton method

--- txtJoiner.py
+++ txtJoiner.py
@@ -4,8 +4,8 @@ import sys #manipulating function arguments
 
 with open("dataset_" + str(date.today()) + ".txt","w+") as dataset:        
     dir = sys.argv[1]
-    files = [os.listdir(dir).sort]
-    
+    files = [os.listdir(dir).sort()]
+
     for file in files :
             lines = open(os.path.join( dir, file ) ,"r").read()
             dataset.write(lines+'\n')
