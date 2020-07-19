c70e5df56418d724723ba44df69bd701d48bc1ba
antonioramiro <antonio.ramiro@tecnico.ulisboa.pt>
2020-07-19 21:13:51 +0100

    debugging txt

--- txtJoiner.py
+++ txtJoiner.py
@@ -5,7 +5,12 @@ import sys #manipulating function arguments
 with open("dataset_" + str(date.today()) + ".txt","w+") as dataset:        
     dir = sys.argv[1]
     files = os.listdir(dir)
-    
-    for file in files.sort():
+    print(type(files))
+    files = files.sort()
+    print(type(files))   
+
+    print(files)
+
+    for file in files :
             lines = open(os.path.join( dir, file ) ,"r").read()
             dataset.write(lines+'\n')
