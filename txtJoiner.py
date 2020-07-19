25beb6d6dda856624349b92c41fb7e2a86eb8f83
antonioramiro <antonio.ramiro@tecnico.ulisboa.pt>
2020-07-19 21:16:28 +0100

    list error fixed

--- txtJoiner.py
+++ txtJoiner.py
@@ -4,13 +4,8 @@ import sys #manipulating function arguments
 
 with open("dataset_" + str(date.today()) + ".txt","w+") as dataset:        
     dir = sys.argv[1]
-    files = os.listdir(dir)
-    print(type(files))
-    files = files.sort()
-    print(type(files))   
-
-    print(files)
-
+    files = [os.listdir(dir).sort]
+    
     for file in files :
             lines = open(os.path.join( dir, file ) ,"r").read()
             dataset.write(lines+'\n')
