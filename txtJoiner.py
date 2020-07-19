0265e4c28a1e103cc69127755178e11699456017
antonioramiro <antonio.ramiro@tecnico.ulisboa.pt>
2020-07-19 21:08:40 +0100

    txtJoiner change to alphabetical order

--- txtJoiner.py
+++ txtJoiner.py
@@ -4,6 +4,7 @@ import sys #manipulating function arguments
 
 with open("dataset_" + str(date.today()) + ".txt","w+") as dataset:        
     dir = sys.argv[1]
-    for file in os.listdir(dir):
+    files = os.listdir(dir).sort()
+    for file in files:
             lines = open( os.path.join( dir, file ) ,"r").read()
             dataset.write(lines+'\n')
