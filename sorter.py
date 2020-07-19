ae3dbb3a866ec69a17b2f818df69ae6113ac4363
antonioramiro <antonio.ramiro@tecnico.ulisboa.pt>
2020-07-19 20:44:15 +0100

    syntax

--- sorter.py
+++ sorter.py
@@ -6,12 +6,12 @@ dataset = {}
 folderSize = len(os.listdir(directory))
 i = 0
 
-while i != folderSize
+while i != folderSize:
     line = os.listdir(directory)[i]
     if not str(line[11:-5]) in dataset:
         dataset[str(line[11:-5])] = [i,0]
     else:
         dataset[str(line[11:-5])][1] = i
-
+    i+=1
 for x, y in dataset.items():
   print(x, y)
