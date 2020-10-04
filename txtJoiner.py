88cbb4dcd40071bf25191bf83c6b7a2a9a456fa9
António Ramiro <antonio.ramiro@tecnico.ulisboa.pt>
2020-10-04 21:25:00 +0100

    filtering images out

--- txtJoiner.py
+++ txtJoiner.py
@@ -3,14 +3,14 @@ from datetime import date #for better organizing results
 import sys #manipulating function arguments
 
 with open("dataset_" + str(date.today()) + ".txt","w+") as dataset:        
-    dire = sys.argv[1]
+        dire = sys.argv[1]
     files = os.listdir(dir)
     files = sorted(files)
     
-    for file in files :
-
-            print('3 file',file)
-            print('3 file',type(file))
+    for file in files:
+        if file[-4:] == '.txt':
+            print(file)
+            print(type(file))
 
             lines = open(os.path.join( dire, file) ,"r").read()
             dataset.write(lines+'\n')
