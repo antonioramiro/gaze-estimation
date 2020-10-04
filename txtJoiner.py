8b2bd4b1e0fc63951a774586a79e99665d5917c4
António Ramiro <antonio.ramiro@tecnico.ulisboa.pt>
2020-10-04 21:25:48 +0100

    Indentation error

--- txtJoiner.py
+++ txtJoiner.py
@@ -3,8 +3,8 @@ from datetime import date #for better organizing results
 import sys #manipulating function arguments
 
 with open("dataset_" + str(date.today()) + ".txt","w+") as dataset:        
-        dire = sys.argv[1]
-    files = os.listdir(dir)
+    dire = sys.argv[1]
+    files = os.listdir(dire)
     files = sorted(files)
     
     for file in files:
