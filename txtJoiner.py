4097be2355cccdb8c582c5b9c2088d8de5bf6191
António Ramiro <antonio.ramiro@tecnico.ulisboa.pt>
2020-10-04 21:22:48 +0100

    Changing sensitive variable name

--- txtJoiner.py
+++ txtJoiner.py
@@ -3,7 +3,7 @@ from datetime import date #for better organizing results
 import sys #manipulating function arguments
 
 with open("dataset_" + str(date.today()) + ".txt","w+") as dataset:        
-    dir = sys.argv[1]
+    dire = sys.argv[1]
     files = os.listdir(dir)
     files = sorted(files)
     
@@ -12,5 +12,5 @@ with open("dataset_" + str(date.today()) + ".txt","w+") as dataset:
             print('3 file',file)
             print('3 file',type(file))
 
-            lines = open(os.path.join( dir, file) ,"r").read()
+            lines = open(os.path.join( dire, file) ,"r").read()
             dataset.write(lines+'\n')
