ff144cd3e3bfbf56607c361b76f7fc3a66cc1b64
António Ramiro <antonio.ramiro@tecnico.ulisboa.pt>
2020-10-04 18:57:17 +0100

    Testing saving the files to root (~)

--- datasetGenerating.py
+++ datasetGenerating.py
@@ -271,10 +271,10 @@ def main(video,VFOA,visualFeedback):
 
                         #outputing to a textfile, note that the chosen name is extremely tailored, manipulating expected inputs
                         # in particular, videos found in ~/source_videos/ folder 
-                        f= open('../dataset/' + str(date.today()) + '_' + sys.argv[1][14:-4]+'_'+sys.argv[2]+'_'+str(j)+'.txt',"w+") 
+                        f= open(str(date.today()) + '_' + sys.argv[1][14:-4]+'_'+sys.argv[2]+'_'+str(j)+'.txt',"w+") 
                         f.writelines(str(resultingVector))
                         f.close()   
-                        if visualFeedback: cv2.imwrite('../dataset/' + str(date.today()) + '_' + sys.argv[1][14:-4]+'_'+sys.argv[2]+'_'+str(j)+'.png', frame)
+                        if visualFeedback: cv2.imwrite( str(date.today()) + '_' + sys.argv[1][14:-4]+'_'+sys.argv[2]+'_'+str(j)+'.png', frame)
                         usedFrames += 1
                         print('This frame has generated data successfully. The ' + VFOA + ' can be found in quadrant ' + str(quadrantVFOA) + '.')
 
